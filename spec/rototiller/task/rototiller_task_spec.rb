118363c75555c909c2e4285dea5674e71c2a6ae9
Eric Thompson <eric.thompson@puppetlabs.com>
2017-04-07 13:22:34 -0700

    (QA-2851) update unit tests to better test against command run changes
    - we can fix up some unit tests after the change to Process.spawn.  We
      also have better handling of ENOENT, update the tests for that.

--- spec/rototiller/task/rototiller_task_spec.rb
+++ spec/rototiller/task/rototiller_task_spec.rb
@@ -152,7 +152,7 @@ module Rototiller::Task
           end
           it 'can override command name with env_var in block as block' do
             # set env first, or command might not have it in time
-            allow(ENV).to receive(:[]).with('BLAH').and_return('my_shiny_new_command')
+            allow(ENV).to receive(:[]).with('BLAH').and_return('echo yeshello')
             task.add_command do |c|
               c.name = 'nonesuch'
               c.add_env do |e|
@@ -161,24 +161,21 @@ module Rototiller::Task
               # this won't yet be set before add_command completes. is this okay?
               #expect(c.name).to eq('my_shiny_new_command')
             end
-            expect(task).to receive(:exit)
             expect{ described_run_task }
-              .to output(/my_shiny_new_command:( command)? not found/)
+              .to output(/yeshello/)
               .to_stdout
           end
           it 'can override command name with env_var in block as hash' do
             # set env first, or command might not have it in time
-            allow(ENV).to receive(:[]).with('BLAH').and_return('my_shiny_new_command')
+            allow(ENV).to receive(:[]).with('BLAH').and_return('echo hellothar')
             task.add_command do |c|
               c.name = 'nonesuch'
               c.add_env({:name => 'BLAH'})
               # this won't yet be set before add_command completes. is this okay?
               #expect(c.name).to eq('my_shiny_new_command')
             end
-            # not sure why this is calling exit twice
-            expect(task).to receive(:exit)
             expect{ described_run_task }
-              .to output(/my_shiny_new_command:( command)? not found/)
+              .to output(/hellothar/)
               .to_stdout
           end
           it 'raises an error when supplied a bad key' do
